b10d3db2cffef259d0fe5054a74ed1610e70035f
Vijay Girase <vijayhgirase2000@gmail.com>
2026-03-26 14:56:10 +0530

    first commit admin css

--- server.js
+++ server.js
@@ -38,16 +38,16 @@ import { productModel } from "./src/models/productModel.js";
 
 app.get('/debug-db', async (req, res) => {
   try {
-    const categoryCount = await categoryModel.countDocuments();
-    const subCategoryCount = await subCategoryModel.countDocuments();
-    const productCount = await productModel.countDocuments();
+    const categories = await categoryModel.find();
+    const subCategories = await subCategoryModel.find();
+    const products = await productModel.find();
     res.json({
       success: true,
       database: mongoose.connection.name,
-      counts: {
-        categories: categoryCount,
-        subCategories: subCategoryCount,
-        products: productCount
+      data: {
+        categories,
+        subCategories,
+        products
       }
     });
   } catch (error) {
